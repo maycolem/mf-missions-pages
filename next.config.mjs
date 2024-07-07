8c760c5b39fbd187ec234380a2387391bb08ddc7
Maycol Christian Elcorrobarrutia Medina <maycol.elcorrobarrutia@apuestatotal.pe>
2024-07-07 12:51:50 -0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+import NextFederationPlugin from "@module-federation/nextjs-mf";
+// this enables you to use import() and the webpack parser
+// loading remotes on demand, not ideal for SSR
+const remotes = (isServer) => {
+  const location = isServer ? "ssr" : "chunks";
+  return {
+    shop: `shop@http://localhost:3002/_next/static/${location}/remoteEntry.js`,
+    checkout: `checkout@http://localhost:3000/_next/static/${location}/remoteEntry.js`,
+  };
 };
+export function webpack(config, options) {
+  config.plugins.push(
+    new NextFederationPlugin({
+      name: "mf-missions-pages",
+      filename: "static/chunks/remoteEntry.js",
+      dts: false,
+      exposes: {},
+      remotes: remotes(options.isServer),
+      shared: {},
+      extraOptions: {
+        exposePages: true,
+      },
+    })
+  );
 
-export default nextConfig;
+  return config;
+}
